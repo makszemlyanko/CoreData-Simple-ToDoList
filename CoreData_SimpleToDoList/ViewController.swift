3a354e1445ee344019cc2f990d88a268ccff41e8
kokosDev <maks.kokos.dev@gmail.com>
2022-08-28 13:22:11 +0300

    Initial Commit

---
+++ CoreData_SimpleToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreData_SimpleToDoList
+//
+//  Created by Maks Kokos on 28.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
